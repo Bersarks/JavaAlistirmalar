f7e84acb8176fe357772ed4c34f77c2a916ba033
Ömer Faruk Tokluoğlu <88704993+Bersarks@users.noreply.github.com>
2023-07-10 09:55:43 +0300

    Add Ders201

---
+++ Ders201/src/Service/PlayerService.java
@@ -0,0 +1,14 @@
+package Service;
+
+import model.Player;
+import model.Team;
+
+import java.util.ArrayList;
+
+public class PlayerService {
+	// yeni bir oyuncu eklenecek
+	// Başlangıçta bir takımı var.
+
+
+	// Transfer oldukça takımı değişecek
+}
